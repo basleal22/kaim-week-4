dec8b766f4de34fceaa4d41c05c80cee36abebfb
basleal22 <solibaslu@gmail.com>
2025-01-03 14:10:25 +0300

    added eda module

---
+++ scripts/eda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+def extract_data(data):
+    csv_data=pd.read_csv(data)
+    return csv_data
+#def Data_cleaning(data):
+    
